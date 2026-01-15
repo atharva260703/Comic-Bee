dab9fd8390c9a65fee4c99688544c832ec635b42
Atharva <atharvapatill2607@gmail.com>
2026-01-15 12:09:10 +0530

    Resolved All issues

--- controllers/comics.controller.js
+++ controllers/comics.controller.js
@@ -1,11 +1,32 @@
-const pool = require("../../db");
-exports.getAllComics = async(req, res) =>{
+const pool = require("../db");
+
+
+exports.getAllComics = async (req, res) => {
     try {
-        const result = await pool.query("SELECT * FROM comics ORDER BY id ASC");
+        const result = await pool.query("SELECT * FROM comics");
         res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to fetch comics",
+            error: err.message
+        });
     }
-    catch(err)
-    {
-        res.status(404).json({"File not Found !!": err.message });
+};
+
+exports.createComic = async (req, res) => {
+    const { title, author } = req.body;
+
+    try {
+        const result = await pool.query(
+            "INSERT INTO comics (title, author) VALUES ($1, $2) RETURNING *",
+            [title, author]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to create comic",
+            error: err.message
+        });
     }
 };
