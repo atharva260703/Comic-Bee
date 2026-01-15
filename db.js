d323fd92bbfa24a4d9d1504db253fdd96f40967d
Atharva <atharvapatill2607@gmail.com>
2026-01-15 15:14:11 +0530

    modified certiicate

--- db.js
+++ db.js
@@ -7,6 +7,10 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 5432,
+    ssl: false,
+    max : 15,
+    idleTimeoutMillis: 20000,
+    connectionTimeoutMillis: 10000,
 });
 
 pool.connect()
