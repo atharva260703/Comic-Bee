7bfbaab1125ffec082c4b8475a2c42ad53084802
Atharva <atharvapatill2607@gmail.com>
2026-04-02 18:31:10 +0530

    Vercel compatible index.js file

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 import dotenv from "dotenv";
-import https from "https";
-import fs from "fs";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
 import pool from "./db.js";
 import comicRoutes from "./routes/comics.routes.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 app.use(
   cors({
-    origin: "https://comicbee.local:3000",
+    origin: "*",
     credentials: true,
   })
 );
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/comics", comicRoutes);
+
 app.get("/comicbee", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
@@ -38,21 +36,9 @@ app.get("/comicbee", async (req, res) => {
     });
   }
 });
-const frontendPath = path.resolve(
-  __dirname,
-  "../Comic-Bee-frontend/Comic-Bee-frontend/dist"
-);
 
-app.use(express.static(frontendPath));
+app.get("/", (req, res) => {
+  res.send("Comic Bee Backend Running");
+});
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
-const httpsOptions = {
-  key: fs.readFileSync("./comicbee.local-key.pem"),
-  cert: fs.readFileSync("./comicbee.local.pem"),
-};
-https.createServer(httpsOptions, app).listen(PORT, "0.0.0.0", () => {
-  console.log(`Comic Bee Server running securely on port ${PORT}`);
-  console.log(`Server started at https://comicbee.local:${PORT}`);
-});
+export default app;
